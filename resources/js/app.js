6ce14ad71c687679e52f3b4bf8787bdd3d8b2ebf
Syed Kounain Abbas Rizvi <abbasmashaddy72@gmail.com>
2022-01-02 10:55:25 +0530

    mostly UI Setup is Done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,29 @@
-require('./bootstrap');
+require('./bootstrap')
+require('fslightbox')
+require('./hope-ui')
+require('./countdown')
 
-import Alpine from 'alpinejs';
+import Alpine from 'alpinejs'
+import Swiper, {
+    Grid,
+    Lazy,
+    EffectFade,
+    Navigation,
+    Pagination,
+    Scrollbar,
+    EffectCoverflow
+} from 'swiper'
 
-window.Alpine = Alpine;
+window.Alpine = Alpine
 
-Alpine.start();
+Alpine.start()
+
+Swiper.use([
+    Grid,
+    Lazy,
+    EffectFade,
+    Navigation,
+    Pagination,
+    Scrollbar,
+    EffectCoverflow
+])
